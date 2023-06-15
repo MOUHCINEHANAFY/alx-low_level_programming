29ee654d675d12a827b727e0d5c72c72552c16f3
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-15 12:22:49 -0700

    task from 1 to 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function of the code to print "programming is like building a multilingual puzzle"
+ * Return : Always 0 if success
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle")
+	
+	return (0);
+
+}
