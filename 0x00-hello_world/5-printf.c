7a151ec8e32cabc5ac21bbbc7f6e1b0ea7b35993
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-15 12:43:49 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print with printf "with proper grammar.."
+ * Return: 0 if succesful program
+ * */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
