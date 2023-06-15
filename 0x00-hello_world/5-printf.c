d03e9984dd880af76c1840484576a9bb8cf44879
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-15 12:45:14 -0700

    recommit of task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - print with printf "with proper grammar.."
  * Return: 0 if succesful program
- * */
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
