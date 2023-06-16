913e89ab984dc4fe7625e202c890e39027e6eb64
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-16 15:24:20 -0700

    task 6 commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this function print from
+ * 0 to 9 with putchar
+ * Return: return 0 if succesfull
+ */
+
+int main(void)
+{
+	int i = 0;
+	while (i < 10)
+	{
+		putchar('0' + i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
