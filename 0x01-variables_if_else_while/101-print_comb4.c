57f8becf8c146877817ae5bfbcc9392fcd89911b
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-16 16:43:15 -0700

    three number combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - print 3 digit combinations
+ * Return: always 0 if succesfull
+ */
+
+int main(void)
+{
+	int first;
+	int second, third;
+
+	for (first = '0'; first <= '7'; first++)
+	{
+		for (second = first + 1; second <= '9'; second++)
+		{
+			for(third = second + 1; third  <= '9'; third++)
+			{
+				putchar(first);
+				putchar(second);
+				putchar(third);
+				if (first == '7' && second  == '8' && third == '9')
+				break;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+return (0);
+}
