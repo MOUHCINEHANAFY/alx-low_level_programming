aaa615e4d593420b42d40ffe2e5a87a50d7094e1
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-20 15:53:10 -0700

    100 commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * print_times_table - print tables
+ * @n: tables times itterance
+ */
+
+void print_times_tables(int n);
+{
+	int a, b, multi_0, multi, t;
+
+		for (a = 0; a <= n; a++)
+		{
+		for (b = 0; b <= n; b++)
+		{
+			multi_0 = a * b;
+			if (multi_0 > 9)
+			{
+				multi = multi_0 % 10;
+				multi_0 /= 10;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(multi_0 + '0');
+				_putchar(multi + '0');
+			}
+			else if (multi_0 > 99)
+			{
+				multi = multi_0 % 10;
+				table = (multi_0 / 10) % 10;
+				multi_0 /= 100;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(multi_0 + '0');
+				_putchar(table + '0');
+				_putchar(multi + '0');
+			}
+			else if (multi_0 == 0 && b == 0)
+			{
+				_putchar(multi_0 + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(multi_0 + '0');
+			}
+		}
+		_putchar('\n');
+		}
+}
