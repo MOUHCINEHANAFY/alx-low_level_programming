fb210156886a77bdd799c9e7e194f1aaff901b2d
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-20 15:28:10 -0700

    task 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add -add two ints & print it
+ * @num1: first int
+ * @num2: second int
+ * Return: sum of num1 & num2
+ */
+
+int add(int num1, int num2)
+{
+	int addition;
+
+	addition = num1 + num2;
+	return (addition);
+}
