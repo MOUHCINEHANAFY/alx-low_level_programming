5a209448efe2f11c3b7eb8d48fee66afd9fb7ba3
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-20 14:23:05 -0700

    task 7 push

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of any number
+ * @n: input number
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+	if(n < 0)
+	{
+	n = (-n);
+	}
+	
+	_putchar((n%10) + '0');
+	return (n%10);
+}
