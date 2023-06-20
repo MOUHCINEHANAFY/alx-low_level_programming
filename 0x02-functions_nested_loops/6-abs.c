45994accf1f92b1b84a60d390ec8bd4b660f0049
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-20 14:08:33 -0700

    task 6 commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - return abslute value of int
+ * @n: input to be calculated
+ * Return: absolut of int number
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
