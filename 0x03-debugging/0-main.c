8432a1e37712831791117a18d31fc2b3a9698e16
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-21 13:17:38 -0700

    debug task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Function used to test ouptu
+ * of positive_or_negative(i) fct
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int X = 0;
+
+	positive_or_negative(X);
+return (0);
+}
