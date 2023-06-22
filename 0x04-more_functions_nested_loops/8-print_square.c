19ed6f6a8415036416c81ef8dd725174e4d5a1a4
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-22 14:23:48 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - This function prints
+ * square using dias '#'
+ * @size: size of square
+ */
+
+void print_square(int size)
+{
+	int hight, width;
+
+	while (n > 0)
+	{
+		for (hight = 0; hight < size; hight++)
+		{
+			for (width = 0; width < size; width++)
+			{
+				_putchar('#');
+			}
+			if (hight == size - 1)
+				continue;
+			_putchar('\n');
+		}
+	break;
+	}
+	_putchar('\n');
+}
