e76c3e9c31d8adbfb139e389b53d56cd010d6e4f
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-22 13:17:49 -0700

    task 5 commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,15 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
+			if (j >= 10)
+			{
+				_putchar((j / 10) + '0');
+				_putchar('0' + (j % 10));
+			}
+			else
+			{
 			_putchar('0' + j);
+			}
 		}
 	_putchar('\n');
 	}
