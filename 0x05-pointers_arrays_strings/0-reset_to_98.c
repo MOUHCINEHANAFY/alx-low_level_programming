59959cc490a3b51740cab1c4fb3c9cb5dc4051cc
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-26 11:03:29 -0700

    Commit for task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - This function update a variable
+ * based on it's pointer
+ * @n: Pointer to get the value 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
