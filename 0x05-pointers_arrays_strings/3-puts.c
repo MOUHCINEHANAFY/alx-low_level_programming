252f1f9cb22d759943e8ee52e4f6ae8695239b31
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-26 11:58:00 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,12 +7,9 @@
 
 void _puts(char *str)
 {
-	int i = 0;
-
-	while (str[i]++)
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
+        while (*str)
+        {
+                _putchar(*str++);
+        }
+        _putchar('\n');
 }
