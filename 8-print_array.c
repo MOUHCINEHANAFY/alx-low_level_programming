6f7f54bd6868fcd217def2cc03ab8ed0c19de7ae
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-26 13:49:51 -0700

    task 8 commit

---
+++ 8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - print array of int
+ * @a: array input
+ * @n: number of elements to be printed from a
+ */
+
+void print_array(int *a, int n)
+{
+	int idx = 0;
+
+	while (idx < n)
+	{
+		printf("%d", a[idx]);
+		idx++;
+	if (idx == n - 1)
+	{
+		continue;
+	}
+	printf(", ");
+	}
+
+	printf("\n");
+}
