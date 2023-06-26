c9933c281492e877b1d256ed47bd4ef30a3ea015
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-26 15:15:40 -0700

    task 101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - calculate code
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int a = 0, b = 0;
+	time_t b;
+
+	srand((unsigned int) time(&t));
+
+	for (; b < 2772;)
+	{
+		a = rand() % 128;
+		if ((b + a) > 2772)
+		{
+			break;
+		}
+		b = b + a;
+		printf("%c", a);
+	}
+
+	printf("%c\n", (2772 - b));
+
+	return (0);
+}
