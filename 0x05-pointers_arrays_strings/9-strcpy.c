556bb49c0552c298a89828e83375d91b053a29d2
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-26 14:05:37 -0700

    task 9 commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - This function copy string pointed by src 
+ * to dest include null character
+ * @dest: destination buffer
+ * @src: source string
+ * Return: destination string pointer
+ */
+
+char *_strcpy(char *dest, const char *src)
+{
+	int idx = 0;
+
+	while (src[idx])
+	{
+	dest[idx] = src[idx];
+	idx = idx + 1;
+	}
+return (dest);
+}
