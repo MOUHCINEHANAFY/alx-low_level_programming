b45231564883312dc777504da372d9258e61062c
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-29 07:44:52 -0700

    task 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - This function incode strings into rot13
+ * @s: input to be encoded
+ * Return: result of string encoded
+ */
+
+char *rot(char *s)
+{
+	int counter = 0, i;
+	char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (s[counter] != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (s[counter] == characters[i])
+			{
+				s[counter] = rot[i];
+				break;
+			}
+		}
+		counter++;
+	}
+
+	return (s);
+}
