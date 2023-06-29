44c028c3bf0f658c644c14884fb82bfb172d38fe
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-29 07:10:38 -0700

    third task

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - Thins function compare 2 strings
+ * @s1: The first string
+ * @s2: ths second string
+ * Return: 0 if two strings are equal
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x = 0;
+
+	for (int i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		x = s1[i] - s2[i];
+		if (x != 0)
+			break;
+	}
+
+	return (x);
+}
