1868a2128f2c12613703d2d3fff03aedb5d59325
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-06-29 08:08:52 -0700

    task 103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -19,39 +19,54 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		length1++;
 	while (n2[length2] != '\0')
 		length2++;
+
 	if (length1 >= length2)
 		big_len = length1;
 	else
 		big_len = length2;
+
 	if (size_r <= big_len + 1)
 		return (0);
+
 	r[big_len + 1] = '\0';
 	length1--, length2--, size_r--;
+
 	num1 = n1[length1] - '0';
 	num2 = n2[length2] - '0';
+
 	while (big_len >= 0)
 	{
 		sum = num1 + num2 + carry;
+
 		if (sum >= 10)
 			carry = sum / 10;
 		else
 			carry = 0;
+
 		if (sum > 0)
 			r[big_len] = (sum % 10) + '0';
 		else
 			r[big_len] = '0';
+
 		if (length1 > 0)
 			length1--, num1 = n1[length1] - '0';
 		else
 			num1 = 0;
+
 		if (length2 > 0)
 			length2--, num2 = n2[length2] - '0';
 		else
 			num2 = 0;
+
 		big_len--, size_r--;
 	}
+
 	if (r[0] == '0')
 		return (r + 1);
 	else
 		return (r);
 }
+
+
+
+
