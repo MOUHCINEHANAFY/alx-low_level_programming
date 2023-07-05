29355fff7c3d190ba986675c79fe80a24857ff7c
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-07-05 12:41:05 -0700

    task 2 correction

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _strlen_recursion -this function aim to retun lengh
+ * _strlen_recursion - this function aim to retun lengh
  * @s: lenght to be returned of this string
  * Return: return lenght
  */
 
-int _strlenght_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 	int lenght = 0;
 
