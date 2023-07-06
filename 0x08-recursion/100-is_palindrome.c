35e7c5699420087b12a4450b84c9c3202f39e3a5
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-07-06 09:42:53 -0700

    commit task 100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * help - first function to help calculating
+ * in main function
+ * @f: char 1
+ * @r: pointer to char 2
+ * @lent: lenght to be calculated
+ * Return: lenght
+ */
+
+int help(char *f, char *r, int lent)
+{
+	if (*f == *(r + lent) && lent > 1)
+	{
+		f++;
+		return (help(f, r, --lent));
+	}
+	if (lent == 1)
+		return (1);
+	return (0);
+}
+
+/**
+ * f_lengh - function to calulate lenght
+ * @str: string to be calculated
+ * @lent: string lenght
+ * Return: lenght
+ */
+
+int f_lengh(char *str, int lent)
+{
+	if (*(str + len) != '\0')
+	{
+		return (f_lengh(str, ++lent));
+	}
+	return (--lent);
+}
+
+/**
+ * is_palindrome - main function
+ * @s: string to be calculated
+ * Return: lenght value
+ */
+
+int is_palindrome(char *s)
+{
+	return (help(s, s, f_lengh(s, 0)));
+}
