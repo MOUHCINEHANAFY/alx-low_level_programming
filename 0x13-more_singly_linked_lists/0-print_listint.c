82cff8d4489f4d824850f4f7fada0f13e129b3b8
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-08-01 15:04:10 -0700

    task 5

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_listint - This function print elm of list and return list size
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,24 +1,19 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * free_listint2 - This function frees a listint_t list.
  * @head: Pointer to adress to list head
  */
+
 void free_listint2(listint_t **head)
 {
 	listint_t *index;
 
-	if (head == NULL)
+	while (head && *head)
 	{
-		return;
+		index = *head;
+		*head = (**head).next;
+		free(index);
 	}
-
-	while (*head)
-	{
-		index = (*head)->next;
-		free(*head);
-		*head = index;
-	}
-
-	head = NULL;
 }
