cbc456ba2f998387e10cbd3962daf464b9c3a5b3
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-08-01 15:36:13 -0700

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,8 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		return (-1);
 	}
-	for (i = 0, listemel = *head; listemel && index
-			; i++, listemel = (*listemel).next)
+	for (i = 0, listemel = *head; listemel && index; i++, listemel = (*listemel).next)
 		if (i == (index - 1))
 		{
 			action = 1;
@@ -45,5 +44,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(*head);
 		return (1);
 	}
-	return(-1)
+
+	return (-1);
 }
