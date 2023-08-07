a134b8e8f2bf866c87e25e7c429e1de6d33beca2
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-08-07 21:02:36 +0000

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,42 +7,31 @@
  * @letters: number of letter to be printed
  * Return: writen letters of 0 if error
  */
-
-ssize_t read_textfile(const char *filename, size_t letters)
+size_t read_textfile(const char *filename, size_t letters)
 {
-	size_t lRead, check;
-	char *buffer;
-	int openf;
+	ssize_t o, r, w;
+	char *buf;
 
-	if (!filename || !letters)
+	if (filename == NULL)
 	{
 		return (0);
 	}
-	buffer = malloc(letters);
-	if (!buffer)
-		return (0);
-
-	openff = open(filename, O_RDONLY);
-	if (openf == -1)
-	{
-		free(buffer);
-		return (0);
-	}
-	lRead = read(openf, buffer, letters);
-	if (lRead < 1)
-	{
-		free(buffer);
-		close(openf);
-		return (0);
-	}
-	check = write(STDOUT_FILENO, buffer, lRead);
-
-	free(buffer);
-	close(openf);
-
-	if (!check || check != lRead)
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 	{
 		return (0);
 	}
-	return (lRead);
+	w = write(STDOUT_FILENO, buf, r);
+	o = open(filename, O_RDONLY);
+	r = read(o, buf, letters);
+
+	if (r == -1 || o == -1 || w == -1 || w != r)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	close(o);
+
+	return (w);
 }
