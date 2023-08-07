0345fbfea7159038d7aad04b071d2f26bd03d4b4
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-08-07 21:18:59 +0000

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * read_textfile - This function read text and print it in posix
@@ -6,24 +7,24 @@
  * @letters: number of letter to be printed
  * Return: writen letters of 0 if error
  */
-size_t read_textfile(const char *filename, size_t letters)
+
+
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
 	char *buf;
 
 	if (filename == NULL)
-	{
 		return (0);
-	}
+
 	buf = malloc(sizeof(char) * letters);
+
 	if (buf == NULL)
-	{
 		return (0);
-	}
-	w = write(STDOUT_FILENO, buf, r);
+
 	o = open(filename, O_RDONLY);
 	r = read(o, buf, letters);
-
+	w = write(STDOUT_FILENO, buf, r);
 	if (r == -1 || o == -1 || w == -1 || w != r)
 	{
 		free(buf);
